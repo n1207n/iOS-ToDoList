c0c13cdbcd5d84385d92011d553d62345ccd9557
Silin Na <n1207n@gmail.com>
2013-11-05 15:04:16 -0800

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Silin Na on 11/5/13.
+//  Copyright (c) 2013 Silin Na. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
